e65820b0e763de090baa3e50071e6199a0b26356
Melvin <melvin.vmegen@gmail.com>
2020-01-09 10:41:37 +0100

    without npm

---
+++ build/precache-manifest.c083d84d0710aba4401acdabc23cc05b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c055bbd21f7f56137d3da77906614cb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f730631583ceb7c426ab",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f6f80787558459bc4aea",
+    "url": "/static/css/main.d292fc83.chunk.css"
+  },
+  {
+    "revision": "f730631583ceb7c426ab",
+    "url": "/static/js/2.68889bc8.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.68889bc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6f80787558459bc4aea",
+    "url": "/static/js/main.dcd7f7c1.chunk.js"
+  },
+  {
+    "revision": "fc3a5ed26f9cd48236e1",
+    "url": "/static/js/runtime-main.d31b52b1.js"
+  },
+  {
+    "revision": "26722cbbb6390a3f9d5485c56bf34eae",
+    "url": "/static/media/CALI 90x90 5184 vis.26722cbb.jpeg"
+  },
+  {
+    "revision": "6acedbf6480c93f6a6e5db821504711b",
+    "url": "/static/media/Crazy Skull 90x90 5184 vis.6acedbf6.jpeg"
+  },
+  {
+    "revision": "33c4be078edf420b2317174718248cf7",
+    "url": "/static/media/Golden Tiger 90x90 5184 vis.33c4be07.jpeg"
+  },
+  {
+    "revision": "e429368a75e3c91525084bc6510ecad3",
+    "url": "/static/media/Signature oeuvres blanc.e429368a.png"
+  },
+  {
+    "revision": "295eff2c404cee6263ae07a6d2cf71a0",
+    "url": "/static/media/TK-Portraitgoldentiger.295eff2c.jpg"
+  },
+  {
+    "revision": "15560cf7c964559a8b550d854e026d94",
+    "url": "/static/media/TKStudio-5.15560cf7.jpg"
+  },
+  {
+    "revision": "a3da1ce0f9ce50b2b6b18e0f915dbc45",
+    "url": "/static/media/Thomas Krief Art Logo Noir.a3da1ce0.png"
+  }
+]);
